ab942f5922209dd252f98bd6303c992022b7ed86
priyal <priyaljaiin52@gmail.com>
2023-02-22 18:54:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./components/Header.js"
+import Meme from "./components/Meme"
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Meme/>
+    </div>
+  );
+}
+
+export default App;
